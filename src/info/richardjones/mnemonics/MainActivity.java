87821fe08a6f15c07908317bb4e6f976cb2230c2
Richard Jones <iamrichardjones@gmail.com>
2016-01-18 06:34:32 +0000

    First commit of very initial first screen

--- src/info/richardjones/mnemonics/MainActivity.java
+++ src/info/richardjones/mnemonics/MainActivity.java
@@ -1,15 +1,33 @@
 package info.richardjones.mnemonics;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 
 public class MainActivity extends Activity {
     /**
-     * Called when the activity is first created.
+     * Entry point....
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        LinearLayout layoutMain = new LinearLayout(this);
+        layoutMain.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layoutMain);
+
+        LayoutInflater inflate = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout layoutTop = (LinearLayout) inflate.inflate(R.layout.mnemonic_input, null);
+        TableLayout layoutBottom = (TableLayout) inflate.inflate(R.layout.results, null);
+
+        TableLayout.LayoutParams relParam = new TableLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutMain.addView(layoutTop);
+        layoutMain.addView(layoutBottom, relParam);
     }
 }
